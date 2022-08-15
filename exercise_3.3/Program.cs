8c06cda5540bd68f3d97fd93b422932fb3c9d5be
Alexander_Napreenko <sasha-napreenko@yandex.ru>
2022-08-15 18:19:40 +0300

    se enter the commit message for your changes. Lines starting

--- exercise_3.3/Program.cs
+++ exercise_3.3/Program.cs
@@ -3,5 +3,5 @@
 int number;
 Console.Write("Введите любое число: ");
 number = Convert.ToInt32(Console.ReadLine());
-for (int i = 0; i <= number; i++) Console.Write($"{Math.Pow(i, 3)} ");
+for (int i = 1; i <= number; i++) Console.Write($"{Math.Pow(i, 3)} ");
 Console.WriteLine(" ");
